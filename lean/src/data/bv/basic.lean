78f3433bbb36bf9f412b6ed8fcb036ddb895b900
Luke Nelson <luke.r.nels@gmail.com>
2021-11-19 10:43:00 -0800

    fix imports

--- lean/src/data/bv/basic.lean
+++ lean/src/data/bv/basic.lean
@@ -3,11 +3,12 @@ Copyright (c) 2021 The UNSAT Group. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Luke Nelson, Xi Wang
 -/
+import data.fin.tuple
 import data.nat.bitwise
 import data.zmod.basic
+import misc.list
 import tactic.omega.main
 import tactic.ring_exp
-import misc.list
 
 /-!
 # Fixed-size bitvectors
