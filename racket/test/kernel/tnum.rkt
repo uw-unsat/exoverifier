27e3ee66d45144312259d8f8d3d449a37b60645c
Luke Nelson <luke.r.nels@gmail.com>
2021-12-04 11:47:32 -0800

    racket: simple tnum test against ll

--- racket/test/kernel/tnum.rkt
+++ racket/test/kernel/tnum.rkt
@@ -2,5 +2,23 @@
 
 (require serval/lib/core
          serval/lib/unittest
+         (prefix-in tnum: "../../lib/tnum.rkt")
          (prefix-in llvm: serval/llvm)
          "../../kernel/bpf/tnum.rkt")
+
+(define (run-basic-test)
+  (parameterize ([llvm:current-machine (llvm:make-machine null null)])
+    (define-symbolic* x y u v (bitvector 64))
+
+    (define a (tnum:tnum x y))
+    (define b (tnum:tnum u v))
+    (define c (tnum:tnum-add a b))
+
+    (define d (apply tnum:tnum (@tnum_add x y u v)))
+
+    (bug-assert (equal? c d) #:msg "test tnum_add")))
+
+(define tnum-tests (test-suite+ "Kernel tnum tests" (test-case+ "simple tests" (run-basic-test))))
+
+(module+ test
+  (time (with-prefer-boolector (run-tests tnum-tests))))
